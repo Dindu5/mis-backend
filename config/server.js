fc623461fe79d6c5e7b8a54dc25910bcc44a3c4c
Dindu5 <chimdindue@gmail.com>
2021-02-09 10:50:48 +0100

    committed all files and folders

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ca5347a5c4b9e5705e09252eb262d780'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5d9b7eac-c3de-49cc-9083-70692fe75f8c'
+};
